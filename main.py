269904da36fafd7f595e996bf7f2d9fdc0cce59e
Yopnalyst <148552504+Yopnalyst@users.noreply.github.com>
2023-10-23 14:58:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
     """
     
     model = xgb.XGBRegressor()
-    model.load_model("C:/Users/YOP DACHALSON/Desktop/CODE PLATEAU/machine learning/CODE PEE PROJECTS/xgb_model.json")
+    model.load_model("xgb_model.json")
     
     st.markdown(html_temp, unsafe_allow_html = True)
     st.write ('')
